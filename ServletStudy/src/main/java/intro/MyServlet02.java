156f0bccd39f050941185199904957e7e02c98e8
YeDongVibe <wkwhstladmss@naver.com>
2023-05-10 17:13:40 +0900

    23.05.10 시작

--- ServletStudy/src/main/java/intro/MyServlet02.java
+++ ServletStudy/src/main/java/intro/MyServlet02.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//@WebServlet("~~")이라고 치면 웹페이지에 hosting된다.
 @WebServlet("/myservlet02")
 public class MyServlet02 extends HttpServlet {
 
