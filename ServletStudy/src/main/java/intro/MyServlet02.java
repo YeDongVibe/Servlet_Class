61bf5db9929477096b367ff120e92c33f996cb50
YeDongVibe <wkwhstladmss@naver.com>
2023-05-10 16:55:44 +0900

    23.05.10 시작

---
+++ ServletStudy/src/main/java/intro/MyServlet02.java
@@ -0,0 +1,27 @@
+package intro;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet02 extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("MyServlet02");
+		resp.setContentType("text/plain; charset=utf-8");
+		PrintWriter out = resp.getWriter();
+		out.println("이것은 서블릿에서 출력하는 문서입니다. 잘보이나요~~~잘보이면 소리질럿!!!!");
+		out.println("잘보이면 성공!축하포카포~~아주 잘하고 있어요^^굿굿베리 굿");
+		out.close();
+	}
+}
