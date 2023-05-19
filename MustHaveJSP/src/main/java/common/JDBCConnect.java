fe443342225ef0161e32516df35a6afd10790887
YeDongVibe <wkwhstladmss@naver.com>
2023-05-19 10:37:20 +0900

    23.05.19 mysql연결

--- MustHaveJSP/src/main/java/common/JDBCConnect.java
+++ MustHaveJSP/src/main/java/common/JDBCConnect.java
@@ -2,14 +2,17 @@ package common;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
 
 public class JDBCConnect {
 
 	private String driver = "com.mysql.cj.jdbc.Driver";
-	private String url="jdbc:mysql://localhost:3306/musthave";
-	private String id="musthave";
-	private String pw="1234";
-	
+	private String url = "jdbc:mysql://localhost:3306/musthave";
+	private String id = "musthave";
+	private String pw = "1234";
+
 	public Connection getConnection() throws Exception {
 		Class.forName(driver);
 		return DriverManager.getConnection(url, id, pw);
@@ -18,7 +21,36 @@ public class JDBCConnect {
 	public static void main(String[] args) throws Exception {
 		JDBCConnect jdbcConnect = new JDBCConnect();
 		Connection con = jdbcConnect.getConnection();
-		
+
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("select * from board");
+
+		try {
+			while (rs.next()) {
+				System.out.println(String.format("%s. %s, %s", rs.getString("title"), rs.getString("content"),
+						rs.getString("id")));
+			}
+			System.out.println();
+			
+			ResultSet rs1 = st.executeQuery("select * from member");
+
+			while (rs1.next()) {
+				System.out.println(
+						String.format("%s. %s, %s", rs1.getString("id"), rs1.getString("pass"), rs1.getString("name")));
+			}
+		}
+
+		catch (SQLException e) {
+			// TODO: handle exception
+		} finally {
+			if (rs != null)
+				rs.close();
+			if (rs1 != null)
+				rs1.close();
+			if (st != null)
+				st.close();
+		}
+
 		con.close();
 
 	}
---
+++ MustHaveJSP/src/main/java/common/JDBCConnection.java
@@ -0,0 +1,68 @@
+package common;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.jsp.JspWriter;
+
+public class JDBCConnection {
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://localhost:3306/musthave";
+	private String id = "musthave";
+	private String pw = "1234";
+
+	public Connection getConnection() throws Exception {
+		Class.forName(driver);
+		return DriverManager.getConnection(url, id, pw);
+	}
+	
+	public void writeMemberToTable(Connection con, JspWriter out) {
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			out.write("<table>");
+			out.write("<tr>");
+			out.write("<td>id</td><td>pass</td><td>name</td><td>regidate</td>");
+			out.write("</tr>");
+			
+			st = con.createStatement();
+			rs = st.executeQuery("select * from member");
+			
+			while(rs.next()) {
+				out.write("<tr>");
+				out.write("<td>"); out.write("id"); out.write("</td>");
+				out.write("<td>"); out.write("pw"); out.write("</td>");
+				out.write("<td>"); out.write("name"); out.write("</td>");
+				out.write("<td>"); out.write("regidate"); out.write("</td>");
+				out.write("</tr>");
+				
+			}
+			out.write("</table>");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (st != null)
+					st.close();
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}	
+	}
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
