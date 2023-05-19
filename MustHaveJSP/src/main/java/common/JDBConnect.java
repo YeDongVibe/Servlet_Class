f22a29ae586789f6c0c3f70511b29adc381cf92f
YeDongVibe <wkwhstladmss@naver.com>
2023-05-19 09:44:05 +0900

    23.05.19

--- MustHaveJSP/src/main/java/common/JDBConnect.java
+++ MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -50,6 +50,7 @@ public class JDBConnect {
 		Connection con  = null;
 		Statement st = null;
 		ResultSet rs = null;
+//		ResultSet rs1 = null;
 		//JDBConnect jdbConnect = new JDBConnect();
 		
 		try {
@@ -57,18 +58,26 @@ public class JDBConnect {
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/musthave", "musthave", "1234");
 			
 			st = con.createStatement();//statement라는 클래스의 객체를 만들어서 st라는 참조변수에 할당함.
-			rs = st.executeQuery("Select sno, pno, jno, qty from spj order by sno");//executeQuery라는 매서드러 데이터베이스에 select~~라는 query를 날림.
+			rs = st.executeQuery("Select id, pass, name from member order by id");//executeQuery라는 매서드러 데이터베이스에 select~~라는 query를 날림.
+//			rs1 = st.executeQuery("Select title,content,id from board");
 			
 			while(rs.next()) {//rs라는 참조변수가 결과값을 저장함. //cursor processing하는 듯한 역할
-				System.out.println(String.format("%s. %s, %s, %d", //String.format이라는 것을 통해 String형태의 값을 댕겨옴. //%s : 문자열, %c : 문자 1개, %d : int, %l : long, %f : float
-						rs.getString("sno"), rs.getString("pno"), rs.getString("jno"), rs.getInt("qty")));
+				System.out.println(String.format("%s. %s, %s", //String.format이라는 것을 통해 String형태의 값을 댕겨옴. //%s : 문자열, %c : 문자 1개, %d : int, %l : long, %f : float
+						rs.getString("id"), rs.getString("pass"), rs.getString("name")));
 				//get~를 통해 각 필드명을 입력하여 데이터를 가져옴. 
 			}
+//			while(rs1.next()) {//rs라는 참조변수가 결과값을 저장함. //cursor processing하는 듯한 역할
+//				System.out.println(String.format("%s. %s, %s", //String.format이라는 것을 통해 String형태의 값을 댕겨옴. //%s : 문자열, %c : 문자 1개, %d : int, %l : long, %f : float
+//						rs.getString("title"), rs.getString("content"), rs.getString("id")));
+//				//get~를 통해 각 필드명을 입력하여 데이터를 가져옴. 
+//			}
+			
 		}catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			try {
-				if (rs != null) rs.close(); //null이 아니면 닫아라.
+				if (rs != null) rs.close();
+//				if(rs1 != null) rs1.close();//null이 아니면 닫아라.
 				if (st != null) st.close();
 				if (con != null) con.close();
 			} catch (SQLException e) {
